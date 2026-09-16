86baa3f5a49768d2786167143e441272ead7c222
TestingAvail <167698435+TestingAvail@users.noreply.github.com>
2026-09-16 21:04:05 +0300

    Update update.py

--- update.py
+++ update.py
@@ -26,7 +26,7 @@ DOMAIN_REGEX = re.compile(
 )
 
 # белые списки, чтобы не заблокировать себе важную хуйню
-WHITELIST = {"localhost", "local", "broadcasthost", "ip6-localhost", "vk.ru"}
+WHITELIST = {"localhost", "local", "broadcasthost", "ip6-localhost", "vk.ru", "cloud.mail.ru"}
 
 
 def fetch_domains():
